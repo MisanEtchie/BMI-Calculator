9f5c819310c5242812172465132f9145560b6316
Misan Etchie <msnetchie11@gmail.com>
2022-12-01 01:41:33 +0100

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Misan Etchie on 01/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BMI
+//
+//  Created by Misan Etchie on 01/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
